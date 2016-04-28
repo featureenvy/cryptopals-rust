a814186d9ecf06a5a3579969895e77fda04051a2
Andreas Arnold <andreas@featureenvy.com>
2016-04-28 20:57:15 +0200

    Add fixed xor function and byte to hex transformation.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub struct Hex<'a>(&'a str);
+pub struct Hex(String);
 pub struct Base64(String);
 
 fn hex_to_byte(hex: char) -> u8 {
@@ -15,7 +15,7 @@ fn hex_to_byte(hex: char) -> u8 {
 }
 
 pub fn hex_to_bytes(hex: &Hex) -> Vec<u8> {
-    let &Hex(raw_input) = hex;
+    let &Hex(ref raw_input) = hex;
     let mut input_iter = raw_input.chars().map(hex_to_byte);
     let mut res = Vec::new();
 
@@ -24,6 +24,31 @@ pub fn hex_to_bytes(hex: &Hex) -> Vec<u8> {
     }
 
     res
+}
+
+fn byte_to_hex(input: &u8) -> (char, char) {
+    let to_char = |byte| match byte {
+        i @ 0...9 => '0' as u8 + i,
+        i @ _ => 'A' as u8 + (i - 10)
+    } as char;
+
+    let first = input >> 4 as u8 & 15;
+    let second = input & 15;
+
+    (to_char(first), to_char(second))
+}
+
+pub fn bytes_to_hex(bytes: &[u8]) -> Hex {
+    let mut res = String::new();
+
+    for (first, second) in bytes.into_iter().map(byte_to_hex) {
+        res.push(first);
+        res.push(second);
+
+        println!("{:?} {}", res, first as u8);
+    }
+
+    Hex(res)
 }
 
 pub fn bytes_to_base64(input: &[u8]) -> Base64 {
@@ -54,6 +79,17 @@ pub fn hex_to_base64(input: &Hex) -> Base64 {
     bytes_to_base64(&bytes)
 }
 
+pub fn xor(input: &Hex, key: &Hex) -> Hex {
+    let input_b = hex_to_bytes(input);
+    let key_b = hex_to_bytes(key);
+
+    let res: Vec<u8> = input_b.into_iter().zip(key_b)
+        .map(|(i, k)| i ^ k)
+        .collect();
+
+    return bytes_to_hex(&res);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,7 +98,7 @@ mod tests {
     fn transforms_from_hex_to_base64() {
         let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
-        let Base64(actual) = hex_to_base64(&Hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
+        let Base64(actual) = hex_to_base64(&Hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string()));
         assert_eq!(expected, actual);
     }
 
@@ -70,9 +106,18 @@ mod tests {
     fn transforms_hex_to_bytes() {
         let expected = [192, 254, 186, 190, 19, 55];
 
-        let actual = hex_to_bytes(&Hex("C0FEBabe1337"));
+        let actual = hex_to_bytes(&Hex("C0FEBabe1337".to_string()));
 
         assert_eq!(expected, &actual[..]);
+    }
+
+    #[test]
+    fn transforms_bytes_to_hex() {
+        let expected = "C0FEBABE1337";
+
+        let Hex(actual) = bytes_to_hex(&[192 as u8, 254, 186, 190, 19, 55]);
+
+        assert_eq!(expected, actual);
     }
 
     #[test]
@@ -82,5 +127,14 @@ mod tests {
         let Base64(actual) = bytes_to_base64(&vec![73, 39, 109]);
 
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn fixed_xor() {
+        let expected = "746865206B696420646F6E277420706C6179";
+
+        let Hex(actual) = xor(&Hex("1c0111001f010100061a024b53535009181c".to_string()), &Hex("686974207468652062756c6c277320657965".to_string()));
+
+        assert_eq!(expected, actual);
     }
 }
